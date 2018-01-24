3d1beadb7ef3ace8584b6ccb1fbb7140b86ea099
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-24 12:56:51 -0600

    add delete cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -7,4 +7,26 @@ class CatsController < ApplicationController
 		@cat = Cat.create(user_id: current_user.id, name: params[:cat][:name], breed: params[:cat][:breed], age: params[:cat][:age], comment: params[:cat][:comment])
 		redirect_to cats_path
 	end
+
+	def show
+		@cat = Cat.find(params[:id])
+	end
+
+	def edit
+		@cat = Cat.find(params[:id]) 
+	end
+
+	def update
+		cat = Cat.find(params[:id])
+		cat.update(name: params[:cat][:name], breed: params[:cat][:breed], age: params[:cat][:age], comment: params[:cat][:comment]) 
+		redirect_to cats_path
+	end
+
+	def destroy
+		cat = Cat.find(params[:id])
+		cat.destroy
+		redirect_to cats_path
+	end
+
+
 end
