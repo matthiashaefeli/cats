8a5fb12886977f289db74e808a971edda6e7eeaa
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-24 11:46:58 -0600

    add model and controller for cat, add more columns to user

---
+++ db/migrate/20180124173124_create_cats.rb
@@ -0,0 +1,13 @@
+class CreateCats < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cats do |t|
+
+	    t.string :name
+	    t.integer :age
+	    t.string :breed
+	    t.string :comment
+
+      t.timestamps
+    end
+  end
+end
