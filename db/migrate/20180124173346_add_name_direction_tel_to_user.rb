2e114858b88f3e765902e60897400f00ad47d4b9
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-24 16:43:06 -0600

    add things to user

--- db/migrate/20180124173346_add_name_direction_tel_to_user.rb
+++ db/migrate/20180124173346_add_name_direction_tel_to_user.rb
@@ -4,9 +4,9 @@ class AddNameDirectionTelToUser < ActiveRecord::Migration[5.1]
   	add_column :users, :last_name, :string
   	add_column :users, :street, :string
   	add_column :users, :city, :string
-  	add_column :users, :zip_code, :integer
+  	add_column :users, :zip_code, :integer, limit: 8
   	add_column :users, :state, :string
-  	add_column :users, :tel, :integer
-  	add_column :users, :mobile, :integer
+  	add_column :users, :tel, :integer, limit: 8
+  	add_column :users, :mobile, :integer, limit: 8
   end
 end
