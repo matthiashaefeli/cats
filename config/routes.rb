225a556490f42c9235a049b811261b1bee7f2c32
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-24 21:16:50 -0600

    send message works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :cats
+  resources :messages
 
 	get '/user/:id', to: 'welcome#usershow', as: 'usershow'
 	post '/userupdate/:id', to: 'welcome#userupdate', as: 'userupdate'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124173346) do
+ActiveRecord::Schema.define(version: 20180125011300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180124173346) do
     t.string "breed"
     t.string "comment"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "from_id"
+    t.integer "to_id"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
