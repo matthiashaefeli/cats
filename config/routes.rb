913bd553cc1aeee16a4924e8f070f1aaeb6263d4
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-25 05:59:52 -0600

    add map controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :cats
   resources :messages
+  resources :map
 
 	get '/user/:id', to: 'welcome#usershow', as: 'usershow'
 	post '/userupdate/:id', to: 'welcome#userupdate', as: 'userupdate'
