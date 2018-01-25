c3541363672e235aacf8557b417fffdbe311d6f2
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-25 07:06:33 -0600

    add geocoder and map index

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,2 +1,15 @@
 module MapHelper
+
+	def user_array
+		array = []
+		users = User.all
+		users.each do |user|
+			if user.id != current_user.id
+				array.push([user.latitude, user.longitude, user.email])
+			end
+		end
+		array
+	end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :cats
+
+  validates_uniqueness_of :email
+
+  geocoded_by :street   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
  
 
   devise :database_authenticatable, :registerable,
