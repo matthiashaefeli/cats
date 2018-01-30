8d8990855c19504a51d99bd4fefaa2cc091da597
matthiashaefeli <haefeli.matthias@gmail.com>
2018-01-30 09:19:12 -0600

    add carrierwave

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,7 +4,8 @@ class CatsController < ApplicationController
 	end
 
 	def create
-		@cat = Cat.create(user_id: current_user.id, name: params[:cat][:name], breed: params[:cat][:breed], age: params[:cat][:age], comment: params[:cat][:comment])
+
+		@cat = Cat.create(avatar: params[:cat][:image], user_id: current_user.id, name: params[:cat][:name], breed: params[:cat][:breed], age: params[:cat][:age], comment: params[:cat][:comment])
 		redirect_to cats_path
 	end
 
--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+require 'carrierwave/orm/activerecord'
 module Cats
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
