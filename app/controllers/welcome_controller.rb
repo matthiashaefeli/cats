b8272e3844ac505900f081ad1dea7c5643df1edc
matthiashaefeli <haefeli.matthias@gmail.com>
2018-02-03 12:03:11 -0600

    css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,36 +4,16 @@ class WelcomeController < ApplicationController
 	end
 
 	def userupdate
+			current_user.update(first_name: params[:user][:first_name],
+												 last_name: params[:user][:last_name],
+												 street: params[:user][:street],
+												 city: params[:user][:city],
+												 zip_code: params[:user][:zip_code],
+												 state: params[:user][:state],
+												 mobile: params[:user][:mobile],
+												 getcat: params[:user][:getcat]
+												 )
 
-		if params[:user][:first_name]
-			current_user.update(first_name: params[:user][:first_name])
-		elsif
-			params[:user][:last_name]
-			current_user.update(last_name: params[:user][:last_name])
-		elsif 
-			params[:user][:street]
-			current_user.update(street: params[:user][:street])
-		elsif 
-			params[:user][:city]
-			current_user.update(city: params[:user][:city])
-		elsif
-			params[:user][:zip_code]
-			current_user.update(zip_code: params[:user][:zip_code])
-		elsif 
-			params[:user][:state]
-			current_user.update(state: params[:user][:state])
-		elsif 
-			params[:user][:tel]
-			current_user.update(tel: params[:user][:tel])
-		elsif
-			params[:user][:mobile]
-			current_user.update(mobile: params[:user][:mobile])
-		elsif 
-			params[:user][:getcat]
-			current_user.update(getcat: params[:user][:getcat])
-
-
-		end
 		redirect_to usershow_path
 	end
 
